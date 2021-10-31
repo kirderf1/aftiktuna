2c28b877323ada3119eb3e42831fa65eddd0e3c8
kirderf1 <kirderf4@gmail.com>
2021-10-31 20:05:59 +0100

    Move functions up from GameObject to Aftik

--- src/main/java/me/kirderf/aftiktuna/level/GameObject.java
+++ src/main/java/me/kirderf/aftiktuna/level/GameObject.java
@@ -1,11 +1,8 @@
 package me.kirderf.aftiktuna.level;
 
-import me.kirderf.aftiktuna.OptionalFunction;
-import me.kirderf.aftiktuna.level.object.door.Door;
 import me.kirderf.aftiktuna.level.object.ObjectType;
+import me.kirderf.aftiktuna.level.object.door.Door;
 
-import java.util.Comparator;
-import java.util.Locale;
 import java.util.Optional;
 
 public abstract class GameObject {
@@ -45,30 +42,13 @@ public abstract class GameObject {
 		this.position = pos;
 	}
 	
-	public final boolean tryMoveTo(int coord) {
-		if(coord != this.getCoord()) {
-			Optional<GameObject> blocking = findBlockingTo(coord);
-			if(blocking.isPresent()) {
-				System.out.printf("The %s is blocking the way.%n", blocking.get().getType().name().toLowerCase(Locale.ROOT));
-				return false;
-			} else {
-				this.position = getPosition().atCoord(coord);
-				return true;
-			}
-		} else return true;
-	}
-	
-	public final boolean isAccessBlocked(int coord) {
-		return findBlockingTo(coord).isPresent();
-	}
-	
-	private Optional<GameObject> findBlockingTo(int coord) {
-		return getRoom().findBlockingInRange(getPosition().getPosTowards(coord).coord(), coord);
-	}
-	
 	public final void teleport(Position pos) {
 		remove();
 		pos.room().addObject(this, pos);
+	}
+	
+	public final void teleport(int coord) {
+		position = position.atCoord(coord);
 	}
 	
 	public boolean isItem() {
@@ -83,17 +63,8 @@ public abstract class GameObject {
 		return Optional.empty();
 	}
 	
-	public final <T> Optional<T> findNearest(OptionalFunction<GameObject, T> mapper) {
-		return getRoom().objectStream().sorted(blockingComparator().thenComparing(Room.byProximity(getCoord())))
-				.flatMap(mapper.toStream()).findFirst();
-	}
-	
 	public final void remove() {
 		getRoom().removeObject(this);
 		position = null;
-	}
-	
-	private Comparator<GameObject> blockingComparator() {
-		return Comparator.comparing(value -> isAccessBlocked(value.getCoord()), Boolean::compareTo);
 	}
 }
--- src/main/java/me/kirderf/aftiktuna/level/object/Aftik.java
+++ src/main/java/me/kirderf/aftiktuna/level/object/Aftik.java
@@ -1,9 +1,10 @@
 package me.kirderf.aftiktuna.level.object;
 
+import me.kirderf.aftiktuna.OptionalFunction;
 import me.kirderf.aftiktuna.level.GameObject;
+import me.kirderf.aftiktuna.level.Room;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Aftik extends GameObject {
@@ -26,5 +27,35 @@ public class Aftik extends GameObject {
 			String itemList = inventory.stream().map(ObjectType::name).collect(Collectors.joining(", "));
 			System.out.printf("Inventory: %s%n", itemList);
 		}
+	}
+	
+	public final boolean tryMoveTo(int coord) {
+		if(coord != this.getCoord()) {
+			Optional<GameObject> blocking = findBlockingTo(coord);
+			if(blocking.isPresent()) {
+				System.out.printf("The %s is blocking the way.%n", blocking.get().getType().name().toLowerCase(Locale.ROOT));
+				return false;
+			} else {
+				teleport(coord);
+				return true;
+			}
+		} else return true;
+	}
+	
+	public final boolean isAccessBlocked(int coord) {
+		return findBlockingTo(coord).isPresent();
+	}
+	
+	protected final Optional<GameObject> findBlockingTo(int coord) {
+		return getRoom().findBlockingInRange(getPosition().getPosTowards(coord).coord(), coord);
+	}
+	
+	public final <T> Optional<T> findNearest(OptionalFunction<GameObject, T> mapper) {
+		return getRoom().objectStream().sorted(blockingComparator().thenComparing(Room.byProximity(getCoord())))
+				.flatMap(mapper.toStream()).findFirst();
+	}
+	
+	private Comparator<GameObject> blockingComparator() {
+		return Comparator.comparing(value -> isAccessBlocked(value.getCoord()), Boolean::compareTo);
 	}
 }
