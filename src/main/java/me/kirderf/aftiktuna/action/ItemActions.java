3ab648898108b8394cbce79a4f723826ecb25733
kirderf1 <kirderf4@gmail.com>
2021-11-03 14:05:37 +0100

    Helper function to search for an available item

--- src/main/java/me/kirderf/aftiktuna/action/ItemActions.java
+++ src/main/java/me/kirderf/aftiktuna/action/ItemActions.java
@@ -8,6 +8,7 @@ import me.kirderf.aftiktuna.level.object.entity.Aftik;
 import me.kirderf.aftiktuna.level.object.entity.Entity;
 
 import java.util.Optional;
+import java.util.function.Consumer;
 
 import static me.kirderf.aftiktuna.action.ActionHandler.argument;
 import static me.kirderf.aftiktuna.action.ActionHandler.literal;
@@ -22,59 +23,59 @@ public final class ItemActions {
 	
 	private static int takeItem(GameInstance game, ObjectType type) {
 		Aftik aftik = game.getAftik();
+		return searchForAndIfNotBlocked(game, aftik, type,
+				item -> takeItem(game, aftik, item),
+				() -> game.out().printf("There is no %s here to pick up.%n", type.lowerCaseName()));
+	}
+	
+	private static void takeItem(GameInstance game, Aftik aftik, Item item) {
+		Optional<Entity.MoveFailure> failure = aftik.moveAndTake(item);
+		
+		if(failure.isEmpty()) {
+			game.out().printf("You picked up the %s.%n", item.getType().lowerCaseName());
+		} else {
+			ActionHandler.printMoveFailure(game, failure.get());
+		}
+	}
+	
+	private static int wieldItem(GameInstance game, WeaponType weaponType) {
+		Aftik aftik = game.getAftik();
+		
+		if (aftik.wieldFromInventory(weaponType)) {
+			game.out().printf("You wielded a %s.%n", weaponType.lowerCaseName());
+			return 1;
+		} else {
+			return searchForAndIfNotBlocked(game, aftik, weaponType,
+					item -> wieldItem(game, aftik, item, weaponType),
+					() -> game.out().printf("There is no %s that you can wield.%n", weaponType.lowerCaseName()));
+		}
+	}
+	
+	private static void wieldItem(GameInstance game, Aftik aftik, Item item, WeaponType type) {
+		Optional<Entity.MoveFailure> failure = aftik.moveAndWield(item, type);
+		if(failure.isEmpty()) {
+			game.out().printf("You picked up and wielded the %s.%n", type.lowerCaseName());
+		} else {
+			ActionHandler.printMoveFailure(game, failure.get());
+		}
+	}
+	
+	private static int searchForAndIfNotBlocked(GameInstance game, Aftik aftik, ObjectType type, Consumer<Item> onSuccess, Runnable onNoMatch) {
 		Optional<Item> optionalItem = aftik.findNearest(Item.CAST.filter(type::matching));
-		if(optionalItem.isPresent()) {
-			
+		if (optionalItem.isPresent()) {
 			Item item = optionalItem.get();
+			
 			Optional<GameObject> blocking = aftik.findBlockingTo(item.getCoord());
 			if (blocking.isEmpty()) {
-				Optional<Entity.MoveFailure> move = aftik.moveAndTake(item);
-				
-				if(move.isEmpty()) {
-					game.out().printf("You picked up the %s.%n", type.lowerCaseName());
-				} else {
-					ActionHandler.printMoveFailure(game, move.get());
-				}
-				
+				onSuccess.accept(item);
 				return 1;
 			} else {
 				ActionHandler.printBlocking(game, blocking.get());
 				return 0;
 			}
 		} else {
-			game.out().printf("There is no %s here to pick up.%n", type.lowerCaseName());
+			onNoMatch.run();
 			return 0;
-		}
-	}
-	
-	private static int wieldItem(GameInstance game, WeaponType itemType) {
-		Aftik aftik = game.getAftik();
-		
-		if (aftik.wieldFromInventory(itemType)) {
-			game.out().printf("You wielded a %s.%n", itemType.lowerCaseName());
-			return 1;
-		} else {
-			Optional<Item> optionalItem = aftik.findNearest(Item.CAST.filter(itemType::matching));
-			if(optionalItem.isPresent()) {
-				
-				Item item = optionalItem.get();
-				Optional<GameObject> blocking = aftik.findBlockingTo(item.getCoord());
-				if (blocking.isEmpty()) {
-					Optional<Entity.MoveFailure> move = aftik.moveAndWield(item, itemType);
-					if(move.isEmpty()) {
-						game.out().printf("You picked up and wielded the %s.%n", itemType.lowerCaseName());
-					} else {
-						ActionHandler.printMoveFailure(game, move.get());
-					}
-					return 1;
-				} else {
-					ActionHandler.printBlocking(game, blocking.get());
-					return 0;
-				}
-			} else {
-				game.out().printf("There is no %s that you can wield.%n", itemType.lowerCaseName());
-				return 0;
-			}
 		}
 	}
 }
