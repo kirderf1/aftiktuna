c16e05768a49b618d4040f93ce01eb0883bb4832
kirderf1 <kirderf4@gmail.com>
2021-11-18 16:48:19 +0100

    Mint is a nicer name; put Mint first in the crew!

--- src/main/java/me/kirderf/aftiktuna/Crew.java
+++ src/main/java/me/kirderf/aftiktuna/Crew.java
@@ -20,8 +20,8 @@ public final class Crew {
 	
 	Crew() {
 		ship = new Ship();
-		crewMembers = new ArrayList<>(List.of(new Aftik("Cerulean", new Stats(9, 2, 10), this),
-				new Aftik("Mint", new Stats(10, 3, 8), this)));
+		crewMembers = new ArrayList<>(List.of(new Aftik("Mint", new Stats(10, 3, 8), this),
+				new Aftik("Cerulean", new Stats(9, 2, 10), this)));
 		aftik = crewMembers.get(0);
 		crewMembers.forEach(aftik1 -> ship.getRoom().addObject(aftik1, 0));
 	}
