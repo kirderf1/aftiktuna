5580cd677a60e7a0b166e3c2b6ccb4c074933531
kirderf1 <kirderf4@gmail.com>
2022-01-27 17:46:52 +0100

    Introduce module game

---
+++ aftiktuna-rust/src/game.rs
@@ -0,0 +1,69 @@
+
+pub struct GOType {
+    symbol : char,
+    name : String
+}
+
+impl GOType {
+    pub fn new(symbol : char, name : &str) -> GOType {
+        GOType {
+            symbol,
+            name: String::from(name)
+        }
+    }
+}
+
+pub struct Area {
+    size : usize,
+    objects : Vec<GameObject>
+}
+
+impl Area {
+    pub fn new(size : usize) -> Area {
+       Area {
+           size,
+           objects : Vec::new()
+       }
+    }
+
+    pub fn add(&mut self, pos : usize, obj_type : GOType) {
+        if pos >= self.size {
+            panic!("Position {} is out of bounds for room with size {}.", pos, self.size);
+        }
+        self.objects.push(GameObject::new(obj_type, pos))
+    }
+
+    pub fn print_area(&self) {
+        let mut symbols = init_symbol_vector(self.size);
+        for obj in &self.objects {
+            symbols[obj.pos] = obj.obj_type.symbol;
+        }
+        println!("{}", String::from_iter(symbols.iter()));
+        for obj in &self.objects {
+            let t = &obj.obj_type;
+            println!("{}: {}", t.symbol, t.name);
+        }
+    }
+}
+
+struct GameObject {
+    obj_type : GOType,
+    pos : usize
+}
+
+impl GameObject {
+    pub fn new(obj_type : GOType, pos : usize) -> GameObject {
+        GameObject {
+            obj_type,
+            pos
+        }
+    }
+}
+
+fn init_symbol_vector(size : usize) -> Vec<char> {
+    let mut symbols = Vec::with_capacity(size);
+    for _ in 0..size {
+        symbols.push('_');
+    }
+    symbols
+}
