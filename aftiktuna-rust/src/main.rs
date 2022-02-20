8dbb14ea5ea95694e8016d61094c94b8bf7f6d93
kirderf1 <kirderf4@gmail.com>
2022-02-20 15:38:01 +0100

    Experimental parse helper

--- aftiktuna-rust/src/main.rs
+++ aftiktuna-rust/src/main.rs
@@ -42,7 +42,7 @@ fn main() {
 
 fn parse_user_action(world: &World, game_state: &GameState) -> Action {
     loop {
-        let input = read_input();
+        let input = read_input().to_lowercase();
 
         match try_parse_input(&input, world, game_state.aftik) {
             Ok(action) => return action,
@@ -52,16 +52,63 @@ fn parse_user_action(world: &World, game_state: &GameState) -> Action {
 }
 
 fn try_parse_input(input: &str, world: &World, aftik: Entity) -> Result<Action, String> {
-    if input.eq_ignore_ascii_case("take fuel can") {
+    if input.eq("take fuel can") {
         parse_take_fuel_can(world, aftik)
-    } else if input.eq_ignore_ascii_case("enter door") {
-        parse_enter_door(world, "door", aftik)
-    } else if input.eq_ignore_ascii_case("enter left door") {
-        parse_enter_door(world, "left door", aftik)
-    } else if input.eq_ignore_ascii_case("enter right door") {
-        parse_enter_door(world, "right door", aftik)
+    } else if let Some(result) = parse_enter(Parse::new(input), world, aftik) {
+        result
     } else {
         Err(format!("Unexpected input: \"{}\"", input))
+    }
+}
+
+fn parse_enter(parse: Parse, world: &World, aftik: Entity) -> Option<Result<Action, String>> {
+    parse
+        .literal("enter")?
+        .match_remaining(&["door", "left door", "right door"], |door_type| {
+            parse_enter_door(world, door_type, aftik)
+        })
+}
+
+struct Parse<'a> {
+    input: &'a str,
+}
+
+impl<'a> Parse<'a> {
+    fn new(input: &str) -> Parse {
+        Parse { input }
+    }
+
+    fn literal(&self, word: &str) -> Option<Parse<'a>> {
+        if self.input.starts_with(word) {
+            Some(Parse {
+                input: self.input.split_at(word.len()).1.trim_start(),
+            })
+        } else {
+            None
+        }
+    }
+
+    fn match_remaining<T, U>(&self, words: &[&str], closure: T) -> Option<U>
+    where
+        T: FnOnce(&str) -> U,
+    {
+        for word in words {
+            if self.input.eq(*word) {
+                return Some(closure(word));
+            }
+        }
+        None
+    }
+
+    fn done<T, U>(&self, closure: T) -> Option<U>
+    where
+        T: FnOnce() -> U,
+    {
+        if self.input.is_empty() {
+            Some(closure())
+        } else {
+            None
+        }
     }
 }
 
