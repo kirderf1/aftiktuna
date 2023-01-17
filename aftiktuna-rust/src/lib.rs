e2268a9c8c3cd5d817367727e65df4e879016f7e
kirderf1 <kirderf4@gmail.com>
2023-01-17 14:19:58 +0100

    [RUST] Split off library crate

---
+++ aftiktuna-rust/src/lib.rs
@@ -0,0 +1,23 @@
+use std::io;
+use std::io::Write;
+
+mod action;
+mod ai;
+mod area;
+mod command;
+pub mod game_loop;
+mod item;
+mod position;
+mod status;
+mod view;
+
+pub fn read_input() -> String {
+    print!("> ");
+    io::stdout().flush().expect("Failed to flush stdout");
+
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read input");
+    String::from(input.trim())
+}
--- aftiktuna-rust/src/main.rs
+++ aftiktuna-rust/src/main.rs
@@ -1,28 +1,6 @@
-use std::io;
-use std::io::Write;
-
-mod action;
-mod ai;
-mod area;
-mod command;
-mod game_loop;
-mod item;
-mod position;
-mod status;
-mod view;
+use aftiktuna::game_loop;
 
 fn main() {
     println!("Welcome to aftiktuna!");
     game_loop::run();
-}
-
-fn read_input() -> String {
-    print!("> ");
-    io::stdout().flush().expect("Failed to flush stdout");
-
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read input");
-    String::from(input.trim())
 }
