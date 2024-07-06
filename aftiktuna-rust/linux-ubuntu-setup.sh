3daca0c7d1c618d1f61b8378636b381fc87292d9
kirderf1 <kirderf4@gmail.com>
2024-07-06 19:22:02 +0200

    sudo for libgtk install in setup script

--- aftiktuna-rust/linux-ubuntu-setup.sh
+++ aftiktuna-rust/linux-ubuntu-setup.sh
@@ -4,4 +4,4 @@ sudo apt-get update
 # Dependencies needed by "macroquad"
 sudo apt-get install pkg-config libx11-dev libxi-dev libgl1-mesa-dev libasound2-dev
 # Dependency needed by the GTK backend of "rfd"
-apt install libgtk-3-dev
+sudo apt install libgtk-3-dev
