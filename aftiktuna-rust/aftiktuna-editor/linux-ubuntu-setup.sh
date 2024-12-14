049dd7630c864b86e51ef9709a4416a2fe071618
kirderf1 <kirderf4@gmail.com>
2024-12-14 19:01:54 +0100

    Set up cargo workspace and separate out aftiktuna-editor package

--- aftiktuna-rust/aftiktuna-editor/linux-ubuntu-setup.sh
+++ aftiktuna-rust/aftiktuna-editor/linux-ubuntu-setup.sh
@@ -3,3 +3,5 @@
 sudo apt-get update
 # Dependencies needed by "macroquad"
 sudo apt-get install pkg-config libx11-dev libxi-dev libgl1-mesa-dev libasound2-dev
+# Dependency needed by the GTK backend of "rfd"
+sudo apt install libgtk-3-dev
