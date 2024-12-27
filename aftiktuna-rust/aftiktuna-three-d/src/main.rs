fe2fe2a2bdc7915647b2fb90abe20cbcb7f50a69
kirderf1 <kirderf4@gmail.com>
2024-12-27 17:06:32 +0100

    Draw a background texture

--- aftiktuna-rust/aftiktuna-three-d/src/main.rs
+++ aftiktuna-rust/aftiktuna-three-d/src/main.rs
@@ -1,3 +1,6 @@
+use aftiktuna::asset::background::BGData;
+use aftiktuna::asset::{background, TextureLoader};
+use aftiktuna::core::area::BackgroundId;
 use winit::dpi;
 use winit::event_loop::EventLoop;
 use winit::platform::windows::WindowBuilderExtWindows;
@@ -36,5 +39,64 @@ fn main() {
         false,
     )
     .expect("Unable to create window");
-    window.render_loop(|frame_input| three_d::FrameOutput::default());
+
+    let background = background::load_raw_backgrounds()
+        .unwrap()
+        .get(&BackgroundId::blank())
+        .unwrap()
+        .load(&mut InPlaceLoader(window.gl()))
+        .unwrap();
+
+    window.render_loop(move |frame_input| {
+        let camera = three_d::Camera::new_2d(frame_input.viewport);
+
+        frame_input
+            .screen()
+            .clear(three_d::ClearState::color_and_depth(0., 0., 0., 1., 1.))
+            .render(
+                &camera,
+                get_render_objects_for_background(&background, &frame_input.context),
+                &[],
+            );
+        three_d::FrameOutput::default()
+    });
+}
+
+struct InPlaceLoader(three_d::Context);
+
+impl TextureLoader<three_d::Texture2DRef, ()> for InPlaceLoader {
+    fn load_texture(&mut self, name: String) -> Result<three_d::Texture2DRef, ()> {
+        let path = format!("assets/texture/{name}.png");
+
+        let texture: three_d::CpuTexture = three_d_asset::io::load_and_deserialize(path).unwrap();
+        Ok(three_d::Texture2DRef::from_cpu_texture(&self.0, &texture))
+    }
+}
+
+fn get_render_objects_for_background(
+    background: &BGData<three_d::Texture2DRef>,
+    context: &three_d::Context,
+) -> Vec<impl three_d::Object> {
+    background
+        .primary
+        .0
+        .layers
+        .iter()
+        .map(|layer| {
+            three_d::Gm::new(
+                three_d::Rectangle::new(
+                    context,
+                    three_d::vec2(400., 300.),
+                    three_d::degrees(0.),
+                    800.,
+                    600.,
+                ),
+                three_d::ColorMaterial {
+                    color: three_d::Srgba::WHITE,
+                    texture: Some(layer.texture.clone()),
+                    ..Default::default()
+                },
+            )
+        })
+        .collect()
 }
