1362559d77274c2a898d14616fce774921dafd74
kirderf1 <kirderf4@gmail.com>
2025-07-05 15:05:44 +0200

    Bump major version to account for earlier field background change

--- aftiktuna-rust/CHANGELOG.md
+++ aftiktuna-rust/CHANGELOG.md
@@ -11,6 +11,10 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/).
 
 - Creatures can optionally be wandering
 
+### Changed
+
+- Save format version 4.0, making it incompatible with previous save files
+
 ### Fixed
 
 - Fixed potential issue in command parsing with special symbols
--- aftiktuna-rust/src/serialization.rs
+++ aftiktuna-rust/src/serialization.rs
@@ -9,8 +9,8 @@ use std::io;
 use std::io::{Read, Write};
 
 pub const SAVE_FILE_NAME: &str = "SAVE_FILE";
-const MAJOR_VERSION: u16 = 3;
-const MINOR_VERSION: u16 = 2;
+const MAJOR_VERSION: u16 = 4;
+const MINOR_VERSION: u16 = 0;
 
 fn verify_version(major: u16, minor: u16) -> Result<(), LoadError> {
     if major != MAJOR_VERSION || minor > MINOR_VERSION {
